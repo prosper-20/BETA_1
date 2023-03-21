671a79ff26a98dc950ef7283715097ce09493db3
prosper <edwardprosper001@gmail.com>
2023-03-21 21:55:37 +0100

    'updates'

---
+++ blog/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
+from .models import Post
+
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+
+def about(request):
+    return render(request, 'about.html', {'title': 'About'})
